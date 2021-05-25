3d0d17eaf7b37a472abc373b22d4e1f5d67f1795
Iara000 <81269914+Iara000@users.noreply.github.com>
2021-05-25 20:38:39 -0300

    Update cadastro.js

--- Src/cadastro.js
+++ Src/cadastro.js
@@ -1,35 +1,25 @@
+function salvar(){
+    alert("Processando");
+    var nome = document.getElementById("nome").value;
+    var email= document.getElementById("email").value;
+    var cpf= document.getElementById("cpf").value;
+    var telefone= document.getElementById("telefone").value;
+    var endereco= document.getElementById("endereço").value;
+    var bairro= document.getElementById("bairro").value;
+    var municipio= document.getElementById("Município").value;
+    var password= document.getElementById("password").value;
+    var usuario = new Object();
+    usuario.nome=nome;
+    usuario.email=email;
+    usuario.cpf=cpf;
+    usuario.telefone=telefone;
+    usuario.endereco=endereco;
+    usuario.bairro=bairro;
+    usuario.municipio=municipio;
+    usuario.password=password;
+    var jm = JSON.stringify(usuario);
+    var ls = window.localStorage;
+    ls.setItem("usuarios", jm);
+    
+}
 
-var localStorage = Window.localStorage;
-
-var cadastro = { "email": document.getElementById("email"),
-"nome": document.getElementById('nome'), 
-"cpf": document.getElementById('cpf'), 
-"telefone": document.getElementById('telefone'), 
-"endereço": document.getElementById('endereço'), 
-"bairro": document.getElementById('bairro'), 
-"Município": document.getElementById('Município'), 
-"password": document.getElementById('password'), 
-};
-var registro = JSON.stringify(cadastro);
-var localStorage = Window.localStorage
-localStorage.setItem('registro','cadastro');
-
-
-
-
-
-
-
-var cadastro = {
-    nome= document.getElementById("nome"),
-    email= document.getElementById("email"),
-    cpf= document.getElementById("cpf"),
-    telefone= document.getElementById("telfone"),
-    endereço= document.getElementById("endereço"),
-    bairro= document.getElementById("bairro"),
-    Município= document.getElementById("Município"),
-    password= document.getElementById("password")
-};
-
-var relatorio = JSON.parse(localStorage.getItem('cadastro'));
-function salvar(){}
