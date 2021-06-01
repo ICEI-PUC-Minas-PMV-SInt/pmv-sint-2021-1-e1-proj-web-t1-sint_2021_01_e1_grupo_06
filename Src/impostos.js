90ac33a072375b07de0df70581cf4e440abe2d85
Iara000 <81269914+Iara000@users.noreply.github.com>
2021-06-01 19:14:13 -0300

    Create impostos.js

---
+++ Src/impostos.js
@@ -0,0 +1,22 @@
+var logado = false;
+
+if (localStorage.getItem("acesso") == "true"){
+    logado = true;
+    console.log('entrou');
+};
+
+if (logado != true){
+    alert("Você não está autenticado!");
+    window.location.href = "login.html";
+}
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
