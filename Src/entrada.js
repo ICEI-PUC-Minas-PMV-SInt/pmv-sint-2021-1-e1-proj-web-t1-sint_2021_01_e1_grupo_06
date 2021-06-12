2a1a0212ff62c9623a2ea9d52d05b54e51984046
Iara000 <81269914+Iara000@users.noreply.github.com>
2021-06-12 17:11:59 -0300

    Update entrada.js

--- Src/entrada.js
+++ Src/entrada.js
@@ -1,67 +1,40 @@
- // tela entrada de dados//
 function gravar() {
+
   var imposto = document.getElementById("imposto").value;
-    var despFixas = document.getElementById("despFixas").value;
-    var despVariaveis = document.getElementById("despVariaveis").value;
-    var estVenda = document.getElementById("estVenda").value;
-    var investimento = document.getElementById("investimento").value;
-    var custo = document.getElementById("kwh").value;
-    var margem = document.getElementById("Margem").value;
+  var despFixas = document.getElementById("despFixas").value;
+  var despVariaveis = document.getElementById("despVariaveis").value;
+  var estVenda = document.getElementById("estVenda").value;
+  var investimento = document.getElementById("investimento").value;
+  var custo = document.getElementById("kwh").value;
+  var entrada = new Object();
+  entrada.imposto = parseFloat(imposto);
+  entrada.despFixas = parseFloat(despFixas);
+  entrada.despVariaveis = parseFloat(despVariaveis);
+  entrada.estVenda = parseFloat(estVenda);
+  entrada.investimento = parseFloat(investimento);
+  entrada.custo = parseFloat(custo);
+  var jm = JSON.stringify(entrada);
+  var ls = window.localStorage;
+  ls.setItem("usuarios", jm);
 
-    var im = JSON.stringify(parseFloat(imposto));
-    var ls = window.localStorage;
-    ls.setItem("imposto", im);
+  window.location.href = "geração.html"
+}
+//tela geração//
+function calcular(){
+  var margem = document.getElementById("margem").value;
 
-    var df = JSON.stringify(parseFloat(despFixas));
-    var ls = window.localStorage;
-    ls.setItem("despFixas", df);
+    var mar = JSON.stringify(parseFloat(margem))
+    var ls = window.localStorage
+    ls.setItem("margem", mar)
+    if (margem = true){
+    var jsonEntrada = window.localStorage.getItem('entrada');
+    var entrada = JSON.parse(jsonEntrada);
+    var calculo = entrada.imposto
 
-    var dv = JSON.stringify(parseFloat(despVariaveis));
-    var ls = window.localStorage;
-    ls.setItem("despVariaveis", dv);
+    var pc = JSON.stringify(parseFloat(calculo))
+    var ls = window.localStorage
+    ls.setItem("Preço", pc)
 
-    var ev = JSON.stringify(parseFloat(estVenda));
-    var ls = window.localStorage;
-    ls.setItem("estVenda", ev);
-
-    var inv = JSON.stringify(parseFloat(investimento));
-    var ls = window.localStorage;
-    ls.setItem("investimento", inv);
-
-    var cust = JSON.stringify(parseFloat(custo));
-    var ls = window.localStorage;
-    ls.setItem("custo", cust);
-
-    var mar = JSON.stringify(parseFloat(margem));
-    var ls = window.localStorage;
-    ls.setItem("margem", mar);
-   
+    document.getElementById("preço").innerHTML = localStorage.Preço
   }
-  function calcular(){
-    var preco = (((Number(localStorage.investimento) + Number(localStorage.despFixas)) / Number(localStorage.estVenda)) + Number(localStorage.custo))/
-    ( 1 - ((Number(localStorage.imposto) + Number(localStorage.despVariaveis) + Number(localStorage.margem))) / 100)
-
-    var preco = JSON.stringify(parseFloat(preco));
-    if (localStorage.getItem('Preço') === null) {
-      // Adicionando um array com um objeto no localstorage
-      localStorage.setItem('Preço', JSON.stringify(parseFloat(preco)));
-    } else {
-      // Copiando o array existente no localstorage e adicionando o novo objeto ao final.
-      localStorage.setItem(
-        'Preço',
-        // O JSON.parse transforma a string em JSON novamente, o inverso do JSON.strigify
-        JSON.stringify([
-          ...JSON.parse(localStorage.getItem('Preço')),
-          preco
-        ])
-      );
-    }
-
-    window.location.href = "geração.html"
-  }
-
-
-// tela geração //
-function voltar(){
-  window.location.href = "entrada.html"
 }
